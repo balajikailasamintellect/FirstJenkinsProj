edb71bd7dcf9abd92c7cc0479d41d8bf1de811fc
Balaji Kailasam <balaji.kailasam@intellectdesign.com>
2022-02-04 19:04:39 +0530

    Add files via upload

---
+++ src/main/java/com/map/jenkins/FirstjenkinsClass.java
@@ -0,0 +1,13 @@
+package com.map.jenkins;
+
+public class FirstjenkinsClass {
+	
+	public int add(int a,int b) {
+		return a+b;
+	}
+	
+	public int sub(int a,int b) {
+		return a-b;
+	}
+
+}
---
+++ src/test/java/com/map/jenkins/FirstjenkinsClassYesy.java
@@ -0,0 +1,14 @@
+package com.map.jenkins;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FirstjenkinsClassYesy {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
